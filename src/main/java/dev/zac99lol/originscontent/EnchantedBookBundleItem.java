347a9c62e66b011cd490d02aab90665e8459502e
Zac <gomeyaywhatever@gmail.com>
2025-11-18 18:34:37 +1100

    remove enchanted book bundle tooltip

--- src/main/java/dev/zac99lol/originscontent/EnchantedBookBundleItem.java
+++ src/main/java/dev/zac99lol/originscontent/EnchantedBookBundleItem.java
@@ -1,22 +1,9 @@
 package dev.zac99lol.originscontent;
 
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class EnchantedBookBundleItem extends Item {
     public EnchantedBookBundleItem(Item.Settings settings) {
         super(settings);
-    }
-
-    @Override
-    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-        tooltip.add(Text.translatable("itemTooltip.originscontent.bookBundle").formatted(Formatting.DARK_AQUA));
     }
 }
