c8cb1b046fafde1303766121851cf7eca23b0be4
Zac <gomeyaywhatever@gmail.com>
2026-07-28 18:23:36 +1000

    removed sync stuff

--- src/main/java/dev/zac99lol/originscontent/ModPowers.java
+++ src/main/java/dev/zac99lol/originscontent/ModPowers.java
@@ -10,7 +10,5 @@ public abstract class ModPowers {
         Registry.register(ApoliRegistries.POWER_FACTORY, OriginsContent.id("disable_offhand"), DisableOffhandPower.getFactory());
         Registry.register(ApoliRegistries.POWER_FACTORY, OriginsContent.id("cancel_inventory_actions"), CancelInventoryActionsPower.getFactory());
         Registry.register(ApoliRegistries.POWER_FACTORY, OriginsContent.id("parry"), ParryPower.getFactory());
-        Registry.register(ApoliRegistries.POWER_FACTORY, OriginsContent.id("prevent_sprinting_particles"), PreventSprintingParticlesPower.getFactory());
-        Registry.register(ApoliRegistries.POWER_FACTORY, OriginsContent.id("pose"), PosePower.getFactory());
     }
 }
